436642959fa9e9010cc5a47436ef1ca4c26fb250
Ercan Kurt <ercan257@outlook.de>
2023-10-18 12:24:02 +0200

    Hello World

---
+++ src/main/java/de/htwberlin/demoprojekt/DemoprojektApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.demoprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoprojektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/demoprojekt/DemoprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.demoprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
